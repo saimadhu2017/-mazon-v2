298e36ef8291d1aad7e53ad69b08840a02263e56
saimadhu2017 <madhusai2017@gmail.com>
2022-10-08 22:21:30 +0530

    added routes for signin and signup

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -3,13 +3,20 @@ import Header from '../Common/Header/Header';
 import SignInDescribe from '../Assoc/SignInDescribe/SignInDescribe';
 import SignUpDescribe from '../Assoc/SignUpDescribe/SignUpDescribe';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      <SignInDescribe />
-      <SignUpDescribe />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<h1>Root one is still under construction</h1>} />
+          <Route path='signin' element={<SignInDescribe />} />
+          <Route path='signup' element={<SignUpDescribe />} />
+          <Route path='*' element={<h1>404</h1>} />
+        </Routes>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
